9aca48d70791099e0950f746ca061e404a0965f4
Mirko <mirko.ruether@web.de>
2018-05-04 15:46:00 +0200

    reorganised project

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Mirko on 04.05.2018.
+//
+
+#include <iostream>
+#include <DMatrix.h>
+#include <NeuralNetwork.h>
+
+using namespace std;
+using namespace linalg;
+
+int main() {
+    NeuralNetwork nn();
+
+    DMatrix matrix(2, 2);
+    matrix(0, 0) = 1;
+    matrix(0, 1) = 2;
+    matrix(1, 0) = 3;
+    matrix(1, 1) = 4;
+
+    cout << matrix.getLength() << endl;
+    string s;
+    cin >> s;
+}
---
+++ test/linalg/DMatrix_Test.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Mirko on 04.05.2018.
+//
+
